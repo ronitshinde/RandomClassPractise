08d8cb2fb0ccc781faaa02cd72fd6cc7eafc90db
ronitshinde <rony.shinde753@gmail.com>
2023-04-23 18:15:18 +0530

    [Ronit] Add : UC1-Random function using Next() method

---
+++ RandomPractise/RandomPractise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPractise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            RandomFunctions function = new RandomFunctions();
+            function.NextMethods();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandomPractise/RandomPractise/RandomFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPractise
+{
+    public class RandomFunctions
+    {
+        const int Constant_Number = 5;
+        public void NextMethods()
+        {
+            //Method One - using Next()
+            Random next = new Random(); //object of random class
+            for (int i = 0; i < Constant_Number; i++)
+            {
+                int randomNum = next.Next(); //way to call "Next()" method
+                Console.WriteLine(randomNum);
+            }
+        }
+    }
+}
