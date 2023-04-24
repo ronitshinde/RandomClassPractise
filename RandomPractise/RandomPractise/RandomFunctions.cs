7fbc88fe9aa4a00bd7d86878df96552fb3c01eaf
ronitshinde <rony.shinde753@gmail.com>
2023-04-24 13:12:23 +0530

    [Ronit] Add : UC4-Get random number using NextDouble method

--- RandomPractise/RandomPractise/RandomFunctions.cs
+++ RandomPractise/RandomPractise/RandomFunctions.cs
@@ -11,12 +11,13 @@ namespace RandomPractise
         const int Constant_Number = 5;
         public void NextMethods()
         {
-            //Method Two - using Next(int)
+            //Method Four - using NextDouble()
             Random next = new Random(); //object of random class.
             int i = 0;
             while (i <= Constant_Number) //Using this loop it will print until the condition is satisfied.
             {
-                int randomNum = next.Next(10); //way to call "Next(int)" method will return random values less than 10.
+                double randomNum = next.NextDouble(); //way to call "NextDouble()" method will return random
+                                               //values greater than 0.0 and less than 1.0
                 Console.WriteLine(randomNum);
                 i++;
             }
